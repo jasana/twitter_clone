28e980d010a1528586b05fbc28153296e4a0172a
Jasana Mael <jasanaemily@hotmail.com>
2016-02-09 14:50:11 +1300

    Add working registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,4 +7,41 @@
 
 <h1>Register a new account</h1>
 
+
+<form action="/register" method="post">
+
+	{!! csrf_field() !!}
+
+	<div class="form-group">
+		<label for="name">Full Name: </label>
+		<input class="form-control" type="text" name="name" id="name" placeholder="John Smith">
+	</div>
+	<div class="form-group">
+		<label for="email">E-Mail: </label>
+		<input class="form-control" type="email" name="email" id="email" placeholder="example@mail.com">
+	</div>
+	<div class="form-group">
+		<label for="password">Password: </label>
+		<input class="form-control" type="password" name="password" id="password">
+	</div>
+	<div class="form-group">
+		<label for="password_confirmation">Confirm password: </label>
+		<input class="form-control" type="password" name="password_confirmation" id="password_confirmation">
+	</div>
+	<input class="btn btn-default" type="submit" value="Register!">
+
+</form>
+
+@if(count($errors))
+
+<ul>
+	@foreach($errors->all() as $error)
+		<li>{{ $error }}</li>
+	@endforeach
+</ul>
+
+@endif
+
+
+
 @endsection
