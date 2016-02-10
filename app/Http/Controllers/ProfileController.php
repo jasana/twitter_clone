022a5f6e6be0f61b331cc7b18bb76883d98e5b12
Jasana Mael <jasanaemily@hotmail.com>
2016-02-10 14:57:34 +1300

    rename a bunch of files from account to profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,14 +8,14 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Tweet;
 
-class AccountController extends Controller
+class ProfileController extends Controller
 {
     public function index() {
 
     	// Count the total amount of tweets by this user
     	$totalTweets = \Auth::user()->tweets()->count();
 
-    	return view('account.index', compact('totalTweets'));
+    	return view('profile.index', compact('totalTweets'));
     }
     public function newTweet(Request $request) {
 
@@ -31,6 +31,6 @@ class AccountController extends Controller
     	$newTweet->save();
 
 
-    	return redirect('account');
+    	return redirect('profile');
     }
 }
