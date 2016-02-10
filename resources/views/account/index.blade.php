91723bcc80d1f756aac123f9234786327dc54c80
Jasana Mael <jasanaemily@hotmail.com>
2016-02-10 14:27:44 +1300

    add tweet table, model and insert functionality

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -7,4 +7,34 @@
 
 <h1>Hi there {{ \Auth::user()->name }}</h1>
 
+<h2>Account stats</h2>
+<ul>
+	<li>Total Tweets: {{ $totalTweets }}</li>
+</ul>
+
+<h2>Write a new Tweet</h2>
+
+<form action="/account/new-tweet" method="post">
+
+	{!! csrf_field() !!}
+
+	<div>
+		<label for="content">Tweet: </label>
+		<textarea class="form-control" name="content" id="content" cols="3">{{ old('content') }}</textarea>
+	</div>
+
+	<input class="btn btn-default" type="submit" value="Post">
+</form>
+
+@if(count($errors))
+	<ul>
+		@foreach($errors->all() as $error)
+			<li class="text-danger">{{ $error }}</li>
+		@endforeach
+	</ul>
+@endif
+
+
+
+
 @endsection
